9754ebf16992b30b67fac71466104ef82a9e66aa
CreamPuffx2 <exploit017@gmail.com>
2020-07-15 03:17:15 +0900

    newsapi API 입력

--- news-viewer-1/src/App.js
+++ news-viewer-1/src/App.js
@@ -4,12 +4,23 @@ import axios from 'axios';
 const App = () => {
   const [data, setData] = useState(null);
 
-  const onClick = () => {
-    axios
-      .get('https://jsonplaceholder.typicode.com/todos/1')
-      .then((response) => {
-        setData(response.data);
-      });
+  // const onClick = () => {
+  //   axios
+  //     .get('https://jsonplaceholder.typicode.com/todos/1')
+  //     .then((response) => {
+  //       setData(response.data);
+  //     });
+  // };
+
+  const onClick = async () => {
+    try {
+      const response = await axios.get(
+        'http://newsapi.org/v2/top-headlines?country=kr&apiKey=da7f9812503a4c878a927b196a3e8855',
+      );
+      setData(response.data);
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   return (
